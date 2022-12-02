636b24549be14898998d32eeca3a73f0562f73e0
John Mark Calustre <jmccalustre07@gmail.com>
2022-12-02 18:11:07 +0800

    Changed -lt to -le

--- assig/C06R02.sh
+++ assig/C06R02.sh
@@ -8,8 +8,7 @@ if [ $HRS -le 3 ]; then
 
 	elif [ $HRS -gt 3 ]; then
 		total=$(( 3 * 15 )) #45
-		hrsnew=$(( 3 - HRS )) #3 -12 = -9
-		hrsnew=$(( hrsnew * -1 )) #-9 * -1 = 9
+		hrsnew=$(( HRS - 3 )) #3 -12 = -9
 		total2=$(( hrsnew * 10 )) # 90
 		total=$(( total + total2 )) # 90 + 45 = 135
 	else
